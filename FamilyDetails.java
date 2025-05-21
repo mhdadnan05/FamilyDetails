4c392b2217e0517d630a2dc8fa7e2d0e49fb9ef2
mhdadnan05 <mohammedadnan93901@gmail.com>
2025-05-21 19:30:43 +0530

    Add files via upload

---
+++ FamilyDetails.java
@@ -0,0 +1,17 @@
+public class FamilyDetails{
+	public static void main (String[]args){
+		System.out.println("Name: Rameshkumar");
+		System.out.println("Relation: Father");
+  		System.out.println("Age: 50");
+             	
+		System.out.println("Name: Ankita");
+		System.out.println("Relation: Mother");
+		System.out.println("Age:45");
+
+		System.out.println("Name: Raju");
+		System.out.println("Relation: Brother");
+
+
+		System.out.println("Age: 28");
+	}
+}
